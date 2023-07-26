0231ee3ab416adf3d131b0ee2266f7996c8b52ce
artur <artikpatskun@gmail.com>
2023-07-26 17:29:21 +0300

    fixed config

--- inc/config.example.php
+++ inc/config.example.php
@@ -3,7 +3,7 @@
 define('ADMIN_LOGIN', '');
 define('ADMIN_PASSWORD', '');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASS', '');
