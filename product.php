183659856e8db52fbae901fb1afea80c271aba72
artur <artikpatskun@gmail.com>
2023-09-12 21:40:00 +0300

    product commit

--- product.php
+++ product.php
@@ -1,76 +1,65 @@
 <?php include("inc/header.php") ?>
 
-    <?php 
-        if (!empty($_GET['id'])) {
-            $product = get_product_from_db($_GET["id"]);
-            $extra_images = db_select("product_images", ["id", "image"], ["product_id" => $_GET["id"]]);
-        }
-        else {
-            $product = null;
-            $extra_images = [];
-        }
-    ?>
+<?php 
+    if (!empty($_GET['id'])) {
+        $product = get_product_from_db($_GET["id"]);
+        $extra_images = db_select("product_images", ["id", "image"], ["product_id" => $_GET["id"]]);
+    }
+    else {
+        $product = null;
+        $extra_images = [];
+    }
+?>
 
-    <nav aria-label="breadcrumb" class="mb-5">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.php">Головна</a></li>
-            <li class="breadcrumb-item"><a href="category.php?id=<?php echo $product["category_id"] ?>"><?php echo $product["category_name"] ?></a></li>
-            <li class="breadcrumb-item active" aria-current="page"><?php echo $product["name"] ?></li>
-        </ol>
-    </nav>
-
-        <div class="row gx-4 gx-lg-5 align-items-center">
-            <div class="col-md-4">
-                <img class="card-img-top mb-5 mb-md-0" src="<?php echo img("products/" . $product['image']) ?>" alt="Зображення товару" />
+<div class="site-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 text-center">
+                <img src="<?php echo img("products/" . $product['image']) ?>" alt="Image" class="img-fluid" width="350"> 
                 <div class="carousel-container mt-3">
-                <div class="carousel-container mt-3">
-                    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-theme="dark" style="max-width: 400px; max-height: 50px">
+                    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
                         <?php 
-                            $isFirst = true;
-                            $images_count = count($extra_images);
-                            for ($i = 0; $i < $images_count; $i += 3) { ?>
-                            <div class="carousel-item <?php echo $isFirst ? 'active' : '' ?>" data-bs-interval="5000">
-                                <div class="row align-items-center justify-content-center">
+                        $isFirst = true;
+                        $images_count = count($extra_images);
+                        for ($i = 0; $i < $images_count; $i += 3) { ?>
+                            <div class="carousel-item <?php echo $isFirst ? 'active' : '' ?>">
+                            <div class="row align-items-center justify-content-center">
                                 <?php foreach (array_slice($extra_images, $i, 3) as $extra_image) { ?>
-                                    <div class="col-md-4">
-                                        <img src="<?php echo img("products/" . $extra_image['image']) ?>" class="d-block w-100 img-fluid" alt="...">
-                                    </div>
-                                <?php } ?>
+                                <div class="col-md-4">
+                                    <img src="<?php echo img("products/" . $extra_image['image']) ?>" class="d-block w-100 img-fluid" alt="...">
                                 </div>
+                                <?php } ?>
                             </div>
+                        </div>
                             <?php $isFirst = false;
-                            }
+                        }
                         ?>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                        <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
+                            <span class="sr-only">Next</span>
+                        </a>
                 </div>
-            </div>
         </div>
+            </div>
             <div class="col-md-6">
-                <div class="small mb-1"><?php echo $product['category_name'] ?></div>
-                <h1 class="display-5 fw-bolder"><?php echo $product['name'] ?></h1>
-                <div class="fs-5 mb-3">
-                    <span><?php echo $product['price'] ?>$</span>
-                    <?php echo $product["available"] == 1 ? '<span class="badge bg-success">Є в наявності</span>' : '<span class="badge bg-danger">Товар відсутній</span>' ?>
-                </div>
-                <div class="d-flex">
-                    <button class="btn btn-primary cart-add-product" data-product-id="<?php echo $product['id'] ?>" <?php echo $product["available"] == 1 ? "" : "disabled" ?>>
-                        Купити 
-                        <i class="fas fa-shopping-cart"></i>
-                    </button>
-                </div>
-                <p class="lead"><?php echo $product['description'] ?></p>
-                
+            <h2 class="text-black"><?php echo htmlentities($product['name']) ?></h2>
+            <p><strong class="text-primary h4">$<?php echo htmlentities($product['price']) ?></strong></p>
+            <div class="row ml-1">
+                <a href="#" class="buy-now btn btn-sm btn-primary cart-add-product" data-product-id="<?php echo $product['id'] ?>">Додати у кошик</a>
+                <a href='#' title="Подобається"><i class="fas fa-heart text-danger col-md-6"></i></a>
             </div>
-        </div>
+            <p class="lead"><?php echo $product['description'] ?></p>
+            
 
-<?php include("inc/footer.php") ?> 
+
+        </div>
+    </div>
+</div>
+
+<?php include("inc/footer.php") ?>
